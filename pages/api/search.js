23a22728bfa2551dc6faf365b9563267664df943
Michael Sell <michael.sell@usa.net>
2022-08-08 19:33:20 +0200

    Production Search Correction

--- pages/api/search.js
+++ pages/api/search.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
   let posts;
 
   if (process.env.NODE_ENV === 'production') {
-    posts = require(path.join(process.cwd(), 'cache/data.js')).posts;
+    posts = require('../../cache/data').posts;
   } else {
     const files = fs.readdirSync(path.join('posts'));
 
